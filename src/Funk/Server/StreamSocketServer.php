f05aefd39837a93d7b9ededc0e163e3d9edbe8c0
c9s <yoanlin93@gmail.com>
2015-09-11 19:48:26 +0800

    Improve socket stream with stream select

--- src/Funk/Server/StreamSocketServer.php
+++ src/Funk/Server/StreamSocketServer.php
@@ -8,6 +8,8 @@ class StreamSocketServer
 
     protected $socket;
 
+    protected $connections = [];
+
     public function __construct($bind)
     {
         $this->bind = $bind;
@@ -15,10 +17,71 @@ class StreamSocketServer
         if (!$this->socket) {
             throw new Exception("Can't connect socket: [$errno] $errstr");
         }
+        $this->connections[] = $this->socket;
     }
 
     public function listen(callable $callback)
     {
+
+        while (1) {
+
+            echo "connections:";
+            var_dump( $this->connections );
+
+            $reads = $this->connections;
+            $writes = NULL;
+            $excepts = NULL;
+            $modified = stream_select($reads, $writes, $excepts, 5);
+            if ($modified === false) {
+                break;
+            }
+
+            echo "modified fd:";
+            var_dump( $modified );
+
+
+            echo "reads:";
+            var_dump( $reads ); 
+
+            foreach ($reads as $modifiedRead) {
+
+                if ($modifiedRead === $this->socket) {
+                    $conn = stream_socket_accept($this->socket);
+                    fwrite($conn, "Hello! The time is ".date("n/j/Y g:i a")."\n");
+                    $this->connections[] = $conn;
+                } else {
+                    $data = fread($modifiedRead, 1024);
+                    var_dump($data);
+
+                    if (strlen($data) === 0) { // connection closed
+                        $idx = array_search($modifiedRead, $this->connections, TRUE);
+                        fclose($modifiedRead);
+                        if ($idx != -1) {
+                            unset($this->connections[$idx]);
+                        }
+                    } else if ($data === FALSE) {
+                        echo "Something bad happened";
+                        $idx = array_search($modifiedRead, $this->connections, TRUE);
+                        if ($idx != -1) {
+                            unset($this->connections[$idx]);
+                        }
+                    } else {
+                        echo "The client has sent :"; var_dump($data);
+                        fwrite($modifiedRead, "You have sent :[".$data."]\n");
+                        fclose($modifiedRead);
+
+                        $idx = array_search($modifiedRead, $this->connections, TRUE);
+                        if ($idx != -1) {
+                            unset($this->connections[$idx]);
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+        /*
         while ($conn = stream_socket_accept($this->socket)) {
             print "accepted " . stream_socket_get_name($conn, true) . "\n";
 
@@ -32,5 +95,6 @@ class StreamSocketServer
             fclose($conn);
         }
         fclose($this->socket);
+         */
     }
 }
