1afa9009d1b906e424507810e60bea85527e21da
c9s <yoanlin93@gmail.com>
2015-09-15 12:15:49 +0800

    call parent::call

--- src/Funk/Middleware/ContentNegotiationMiddleware.php
+++ src/Funk/Middleware/ContentNegotiationMiddleware.php
@@ -16,11 +16,10 @@ class ContentNegotiationMiddleware extends Middleware
 
     public function call(array & $environment, array $response)
     {
-        $n = $this->next;
         $accept = isset($environment['HTTP_ACCEPT']) ? $environment['HTTP_ACCEPT'] : '';
         $priorities = isset($environment['negotiation.priorities']) ? $environment['negotiation.priorities'] : array();
         $environment['request.best_format'] = $this->negotiator->getBest($accept, $priorities);
-        return $n($environment, $response);
+        return parent::call($environment, $response);
     }
 }
 
