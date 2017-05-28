2a3dac96014ec03a0fd66c4df8bd0f3eadbc3281
c9s <yoanlin93@gmail.com>
2017-05-28 22:34:59 +0800

    add phpunit test case class alias

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . "/controllers.php";
+
+// This should work for HHVM on travis
+if (!class_exists('PHPUnit\\Framework\\TestCase')) {
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\\Framework\\TestCase');
+}
